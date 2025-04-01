db6a6f81707837e7b9054718a68ff23ec74d1de3
josemar.silva <josemar.silva@senarmt.org.br>
2025-03-31 22:30:23 -0400

    feat(domain): adiciona models  para entidades
    - Cria models:
      - Pessoa, FotoPessoa, PessoaEndereco
      - Unidade, UnidadeEndereco
      - ServidorEfetivo, ServidorTemporario
      - Locacao, Cidade, Endereco

--- app/Models/ServidorTemporario.php
+++ app/Models/ServidorTemporario.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ServidorTemporario extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'servidor_temporario';
+
+    protected $fillable = [
+        'pes_id',
+        'sf_data_admissao',
+        'sf_data_demissao',
+    ];
+
+    public function pessoa()
+    {
+        return $this->belongsTo(Pessoa::class);
+    }
 }
--- app/Models/Unidade.php
+++ app/Models/Unidade.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Unidade extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'unidade';
+
+    protected $primaryKey = 'unid_id';
+
+    protected $fillable = [
+        'unid_id',
+        'unid_nome',
+        'unid_sigla',
+    ];
 }
